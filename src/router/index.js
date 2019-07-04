4fee9a5cc13350d55efa112cb319cb3cd541a4ba
97527027@qq.com <97527027@qq.com>
2019-07-04 19:15:04 +0800

    第一次上传项目

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      component: resolve => require(['@/view/index'], resolve)
+    }
+  ]
+});
+
+export default router;
---
+++ src/view/Index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>ACE 的模板</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
