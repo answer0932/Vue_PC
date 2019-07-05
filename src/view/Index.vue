8c26a6aabfc110005c264a576c6c656fb3f43f9d
97527027@qq.com <97527027@qq.com>
2019-07-05 09:21:50 +0800

    新加友情链接地址

--- src/view/Index.vue
+++ src/view/Index.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
         <h1>ACE 的模板</h1>
+        <p>
+            <a href="https://www.swiper.com.cn/">Swiper中文网</a>
+        </p>
+        <p>
+            <a href="https://daneden.github.io/animate.css/">Animate.css</a>
+        </p>
+        <p>
+            <a href="https://jquery.com/">JQuery</a>
+        </p>
+        <p>
+            <a href="http://momentjs.cn/">Moment.js中文网</a>
+        </p>
     </div>
 </template>
 
